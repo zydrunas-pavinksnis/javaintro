a4a8e5e8f92d7107fd71543b1867e99ba96980ba
Žydrūnas Pavinkšnis <63241045+zydrunas-pavinksnis@users.noreply.github.com>
2020-06-16 23:00:43 +0300

    Isspresta

--- src/MatricuDaugyba.java
+++ src/MatricuDaugyba.java
@@ -1,35 +1,47 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MatricuDaugyba {
 
     public static void main(String[] args) {
 
-        // double [][] m1 = {{2.5, 6}, {5, 0.8}};
+        int [] dydis = matricuDydzioSukurimas();
 
-        // matricosSukurimas(m1);
+        double [][] m1 = new double[dydis[0]][dydis[1]];
+        double [][] m2 = new double[dydis[1]][dydis[2]];
 
-        // atspausdintiMatrica(m1);
+        matricosUzpildymas(m1);
+        matricosUzpildymas(m2);
 
-        matricuDydzioSukurimas();
+        double [][] m3 = matricuDaugyba(m1, m2);
 
+        /* kontrolinis pavyzdys is wiki
+        double [][] m1 = {{3, 1}, {2, 1}, {1, 0}};
+        double [][] m2 = {{1, 0, 2}, {-1, 3, 1}};
+        double [][] m3 = matricuDaugyba(m1, m2);
+        */
 
+        /* kontrolinis pavyzdys is uzdavinio salygos
+        double [][] m1 = {  {2, 3},
+                            {4, 5}};
+        double [][] m2 = {  {1, 2},
+                            {0, 4}};
+        double [][] m3 = matricuDaugyba(m1, m2);
+        double [][] kontrolineMatrica = {   {2*1+3*0, 2*2+3*4},
+                                            {4*1+5*0, 4*2+5*4}};
+        atspausdintiMatrica(kontrolineMatrica);
+        System.out.println();
+        */
+
+        atspausdintiMatrica(m1);
+        System.out.println();
+        atspausdintiMatrica(m2);
+        System.out.println();
+        atspausdintiMatrica(m3);
 
     }
-    /*
-    static double [] dviejuDauginimoMatricuSukurimas() {
 
-        int [] dydziai = matricuDydzioSukurimas();
-
-        double [][] matrica1 = new double[dydziai[0]][dydziai[1]];
-        double [][] matrica2 = new double[dydziai[1]][dydziai[2]];
-
-        return matrica1;
-
-    }
-
-     */
-
-    static int [] matricuDydzioSukurimas() {
+       static int [] matricuDydzioSukurimas() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Matricas galima dauginti, jei jos yra suderintos.");
         System.out.println("1-os matricos stulpeliu skaicius turi sutapti su 2-os matricos eiluciu skaicium.");
@@ -48,27 +60,52 @@ public class MatricuDaugyba {
         return dydziai;
     }
 
-    /*
-    static double atsitiktinioSkaiciausGeneravimas () {
+    static double atsitiktinioSkaiciausGeneravimas() {
+        Random rand = new Random();
+        double skaicius = rand.nextDouble();
+        double teigNeig = rand.nextDouble();
+        double minPlus = 0;
+        skaicius = skaicius * 100;
+        skaicius = Math.round(skaicius);
+        skaicius = skaicius / 10;
+        if (teigNeig < 0.5) {
+            minPlus = -1;
+        } else {
+            minPlus = 1;
+        };
+        skaicius = skaicius * minPlus;
 
-
+        return skaicius;
     }
 
-     */
+    static void matricosUzpildymas (double[][] matrica) {
+        for(int i = 0; i < matrica.length; i++) {
+            for(int j = 0; j < matrica[i].length; j++) {
+                matrica[i][j] = atsitiktinioSkaiciausGeneravimas();
+            }
+        }
+    }
 
-    static void matricosSukurimas(double [][] matrica) {
+    static double[][] matricuDaugyba (double[][]matrica1, double[][]matrica2) {
+        double [][] matrica3 = new double[matrica1.length][matrica2[0].length];
+        int s = matrica1[0].length;
 
+        for(int i = 0; i < matrica3.length; i++) {
+            for (int j = 0; j < matrica3[i].length; j++) {
+                for (int k = 0; k < s; k++) {
+                    matrica3[i][j] = matrica3[i][j] + ( matrica1[i][k] * matrica2[k][j] );
+                }
+            }
+        }
+        return matrica3;
     }
 
     static void atspausdintiMatrica(double [][] matrica) {
-        for (double[] eilute : matrica) {
-            for (double elem : eilute) {
-                System.out.print(elem + "  ");
+        for(int i = 0; i < matrica.length; i++) {
+            for(int j = 0; j < matrica[i].length; j++) {
+                System.out.print(matrica[i][j] + "  ");
             }
             System.out.println();
         }
-
     }
-
-
 }
